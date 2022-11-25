aec807dc1195d04bcf1d6bcfbc567ef89e5d73ca
odinsam <123679@qq.com>
2022-11-25 16:28:03 +0800

    feat: odin flutter dio plugin init

--- ios/Classes/OdindioPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface OdindioPlugin : NSObject<FlutterPlugin>
-@end
--- lib/odindio.dart
+++
@@ -1,8 +0,0 @@
-
-import 'odindio_platform_interface.dart';
-
-class Odindio {
-  Future<String?> getPlatformVersion() {
-    return OdindioPlatform.instance.getPlatformVersion();
-  }
-}
